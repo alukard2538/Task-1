9b8ccacd9b50f834fb74cc4829082407efa0351c
alukard2538 <68911688+alukard2538@users.noreply.github.com>
2021-02-16 10:34:51 +0500

    Add files via upload

---
+++ Family/Person.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Family
+{
+    class Person
+    {
+        public string name { get; }
+        private Person spouse { get; set; }
+        private Person parent { get; }
+        private List<Person> parents_list = new List<Person>();
+        private List<Person> childs_list = new List<Person>();
+
+        public Person(string name, Person parent = null, Person spouse = null)
+        {
+            this.name = name;
+            this.parent = parent;
+            this.spouse = spouse;
+
+            if (parent != null)
+            {
+                this.parent.childs_list.Add(this);
+                this.parents_list.Add(this.parent);
+                if (this.parent.spouse != null)
+                {
+                    this.parent.spouse.childs_list.Add(this);
+                    this.parents_list.Add(this.parent.spouse);
+                }                
+            }
+        }
+
+        public List<Person> GetChilds()
+        {
+            return this.childs_list;
+        }
+
+        public List<Person> GetParents()
+        {            
+            return this.parents_list;
+        }
+
+        public List<Person> GetSiblings()
+        {
+            List<Person> siblings_list = new List<Person>();
+            if (this.parent != null)
+            {
+                foreach (Person parent in this.GetParents())
+                {
+                    foreach (Person sibling in parent.GetChilds())
+                    {
+                        if (sibling != null)
+                        {
+                            if (sibling != this)
+                            {
+                                if (!siblings_list.Contains(sibling))
+                                    siblings_list.Add(sibling);
+                            }
+                        }
+
+                    }
+                }
+                return siblings_list;
+            }
+            else
+                return null;
+            
+        }
+
+        public List<Person> GetUncles()
+        {
+            List<Person> uncles_list = new List<Person>();
+            if (this.parent != null)
+            {
+                foreach (Person parent in this.GetParents())
+                {
+                    if (parent.parent != null)
+                    {
+                        foreach (Person uncle in parent.GetSiblings())
+                        {
+                            if (uncle != null)
+                            {
+                                if (!uncles_list.Contains(uncle))
+                                    uncles_list.Add(uncle);
+                                if (uncle.spouse != null)
+                                {
+                                    if (!uncles_list.Contains(uncle.spouse))
+                                        uncles_list.Add(uncle.spouse);
+                                }
+                            }
+                        }
+                    }                    
+                }
+                return uncles_list;
+            }
+            else
+                return null;
+        }
+
+        public List<Person> GetCousins()
+        {
+            List<Person> cousins_list = new List<Person>();
+            if (this.parent != null)
+            {
+                foreach (Person uncle in this.GetUncles())
+                {
+                    if (uncle != null)
+                    {
+                        foreach (Person cousine in uncle.GetChilds())
+                        {
+                            if (cousine != null)
+                            {
+                                if (!cousins_list.Contains(cousine))
+                                    cousins_list.Add(cousine);
+                            }
+                        }
+                    }
+
+                }
+                return cousins_list;
+            }
+            else
+                return null;            
+        }
+
+        public List<Person> GetInLaw()
+        {
+            List<Person> inlaw_list = new List<Person>();
+            if (this.spouse != null)
+            {
+                if (this.spouse.parent != null)
+                {
+                    foreach (Person inlaw in this.spouse.GetParents())
+                    {
+                        if (inlaw != null)
+                            inlaw_list.Add(inlaw);
+                    }
+                }                
+                return inlaw_list;
+            }
+            else
+                return null;            
+        }
+
+        public static void Wedding(Person a, Person b)
+        {
+            if (a == b)
+                Console.WriteLine(a.name + " can't marry himself");
+            else if (a.spouse != null)
+                Console.WriteLine(a.name + " already has a spouse - " + a.spouse.name);
+            else if (b.spouse != null)
+                Console.WriteLine(b.name + " already has a spouse - " + b.spouse.name);
+            else 
+            {
+                a.spouse = b;
+                b.spouse = a;
+            }                       
+        }              
+    }
+}
---
+++ Family/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Family
+{
+    class Program
+    {
+        static List<string> GetNames(List<Person> members)
+        {
+            List<string> names = new List<string>();
+            if (members.Count != 0)
+            {
+                foreach (Person member in members)
+                {
+                    if (member != null)
+                        names.Add(member.name);
+                }
+                return names;
+            }
+            else
+                return null;
+
+        }
+
+        static void PrintNames(List<Person> members)
+        {
+            if (members.Count != 0)
+            {
+                foreach (Person member in members)
+                {
+                    if (member != null)
+                        Console.Write(member.name + " ");
+                }
+            }
+            else
+                Console.Write("There aren't members");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////////
+            ////////////////////////Making Family////////////////////////////
+            /////////////////////////////////////////////////////////////////
+            //First Generation
+            Person Earendil = new Person("Earendil");
+            Person Elwing = new Person("Elwing");
+            Person.Wedding(Earendil, Elwing);
+
+            Person Celeborn = new Person("Celeborn");
+            Person Galadriel = new Person("Galadriel");
+            Person.Wedding(Celeborn, Galadriel);
+                       
+            //Second Generation
+            Person Elros = new Person("Elros", Earendil);
+            Person Elrond = new Person("Elrond", Earendil);
+
+            Person Celebrian = new Person("Celebrian", Celeborn);
+
+            Person ElrosWife = new Person("ElrosWife");
+
+            Person.Wedding(Elrond, Celebrian);
+            Person.Wedding(Elros, ElrosWife);
+
+            //Third Generation
+            Person Elladan = new Person("Elladan", Elrond);
+            Person Elrohir = new Person("Elrohir", Elrond);
+            Person Arwen = new Person("Arwen", Elrond);
+
+            Person Vardamir = new Person("Vardamir", Elros);
+            Person Atanalcar = new Person("Atanalcar", Elros);
+            Person Tindomiel = new Person("Tindomiel", Elros);
+            /////////////////////////////////////////////////////////////////
+
+
+
+            ///////////////////////////TEST//////////////////////////////////
+            Person Sauron = new Person("Sauron");
+            Person.Wedding(Sauron, Sauron);
+            Person Morgot = new Person("Morgot");
+            Person.Wedding(Sauron, Morgot);
+            Person Saruman = new Person("Saruman");
+            Person.Wedding(Sauron, Saruman);            
+            PrintNames(Saruman.GetParents());
+            PrintNames(Arwen.GetParents());
+            PrintNames(Elrohir.GetUncles());
+            PrintNames(Vardamir.GetCousins());
+            PrintNames(Elrond.GetInLaw());
+            PrintNames(Elrond.GetParents());
+            PrintNames(Elrond.GetParents());
+            PrintNames(Arwen.GetCousins());
+            PrintNames(Arwen.GetCousins());
+            Console.ReadLine();
+        }
+    }
+}
